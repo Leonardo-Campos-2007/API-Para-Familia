4fe88595a4ca316b5f09e15004e5f40f7217e911
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-03-02 16:59:22 -0300

    Apaguei tudo por que o Spring ta bugado.

--- Back-End/api/src/main/java/tech/family/api/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package tech.family.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.family.api.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-}
